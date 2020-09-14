9301cca6a645dd9c6d487f91eddec373c0346f03
Mahdy hamad <mahdy.hamad45@gmail.com>
2020-09-14 09:23:37 +0300

    feat(): added graphql setup

---
+++ src/app/graphql.module.ts
@@ -0,0 +1,38 @@
+import {NgModule} from '@angular/core';
+import {APOLLO_OPTIONS} from 'apollo-angular';
+import {ApolloClientOptions, ApolloLink, InMemoryCache} from '@apollo/client/core';
+import {HttpLink} from 'apollo-angular/http';
+import {setContext} from '@apollo/client/link/context';
+
+const uri = 'localhost:8000/graphql'; // <-- add the URL of the GraphQL server here
+export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+  const basic = setContext((operation, context) => ({
+    headers: {
+      Accept: 'charset=utf-8'
+    }
+  }));
+  const token = localStorage.getItem('token');
+  const auth = setContext((operation, context) => ({
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+  }));
+  const link = ApolloLink.from([basic, auth, httpLink.create({ uri })]);
+  const cache = new InMemoryCache();
+
+  return {
+    link,
+    cache
+  };
+}
+
+@NgModule({
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink],
+    },
+  ],
+})
+export class GraphQLModule {}
